198df127d0c149be1c3d8e7e28b1cf824f1d576b
na-production <77531241+naproduction@users.noreply.github.com>
2026-09-10 00:00:17 +0300

    test

--- ExampleProvider/src/main/kotlin/com/example/StardimaProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/StardimaProvider.kt
@@ -1,4 +1,4 @@
-package com.stardima
+package com.example
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.ExtractorLink
@@ -12,7 +12,6 @@ class StardimaProvider : MainAPI() {
     override var lang = "ar"
     override val supportedTypes = setOf(TvType.Cartoon, TvType.Anime)
 
-    // 1. Home Page Sections
     override val mainPage = mainPageOf(
         "$mainUrl/" to "Latest Releases",
         "$mainUrl/category/cartoons/" to "Cartoons"
@@ -30,14 +29,14 @@ class StardimaProvider : MainAPI() {
         val linkEl = this.selectFirst("a") ?: return null
         val href = fixUrlNull(linkEl.attr("href")) ?: return null
         val title = linkEl.attr("title").ifEmpty { this.select(".title, h2, h3").text() }
-        val poster = fixUrlNull(this.selectFirst("img")?.attr("src") ?: this.selectFirst("img")?.attr("data-src"))
+        val img = this.selectFirst("img")
+        val poster = fixUrlNull(img?.attr("src")?.ifEmpty { null } ?: img?.attr("data-src"))
 
         return newAnimeSearchResponse(title, href, TvType.Cartoon) {
             this.posterUrl = poster
         }
     }
 
-    // 2. Search
     override suspend fun search(query: String): List<SearchResponse> {
         val url = "$mainUrl/?s=$query"
         val document = app.get(url).document
@@ -46,29 +45,32 @@ class StardimaProvider : MainAPI() {
         }
     }
 
-    // 3. Load Episodes
     override suspend fun load(url: String): LoadResponse {
         val document = app.get(url).document
         val title = document.selectFirst("h1.entry-title, h1")?.text() ?: "Episode"
         val poster = document.selectFirst("meta[property=og:image]")?.attr("content")
 
-        val episodes = mutableListOf<Episode>()
+        val episodes = ArrayList<Episode>()
         val episodeElements = document.select(".episodes-list a, .list-episodes a")
 
         if (episodeElements.isNotEmpty()) {
-            episodeElements.forEachIndexed { index, el ->
+            for ((index, el: Element) in episodeElements.withIndex()) {
                 val epHref = fixUrl(el.attr("href"))
                 val epName = el.text().ifEmpty { "Episode ${index + 1}" }
-                episodes.add(newEpisode(epHref) {
-                    this.name = epName
-                    this.episode = index + 1
-                })
+                episodes.add(
+                    newEpisode(epHref) {
+                        this.name = epName
+                        this.episode = index + 1
+                    }
+                )
             }
         } else {
-            episodes.add(newEpisode(url) {
-                this.name = title
-                this.episode = 1
-            })
+            episodes.add(
+                newEpisode(url) {
+                    this.name = title
+                    this.episode = 1
+                }
+            )
         }
 
         return newTvSeriesLoadResponse(title, url, TvType.Cartoon, episodes) {
@@ -76,8 +78,6 @@ class StardimaProvider : MainAPI() {
         }
     }
 
-
-    // 4. Video Stream Extractor
     override suspend fun loadLinks(
         data: String,
         isCasting: Boolean,
@@ -85,10 +85,13 @@ class StardimaProvider : MainAPI() {
         callback: (ExtractorLink) -> Unit
     ): Boolean {
         val document = app.get(data).document
-        val iframes = document.select("iframe").mapNotNull { it.attr("src") }
+        val iframes = document.select("iframe")
 
-        for (iframeUrl in iframes) {
-            loadExtractor(fixUrl(iframeUrl), data, subtitleCallback, callback)
+        for (iframe in iframes) {
+            val src = iframe.attr("src")
+            if (src.isNotEmpty()) {
+                loadExtractor(fixUrl(src), data, subtitleCallback, callback)
+            }
         }
 
         return true
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,11 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // Shitpack repo which contains our tools and dependencies
         maven("https://jitpack.io")
     }
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
@@ -61,7 +59,9 @@ subprojects {
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
-                    "-Xno-receiver-assertions"
+                    "-Xno-receiver-assertions",
+                    "-Xskip-metadata-version-check",
+                    "-Xskip-prerelease-check"
                 )
             }
         }
@@ -74,14 +74,10 @@ subprojects {
         // Stubs for all cloudstream classes
         cloudstream("com.lagradost:cloudstream3:pre-release")
 
-        // These dependencies can include any of those which are added by the app,
-        // but you don't need to include any of them if you don't need them.
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle.kts
+        // These dependencies can include any of those which are added by the app
         implementation(kotlin("stdlib")) // Adds Standard Kotlin Features
         implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP Lib
         implementation("org.jsoup:jsoup:1.18.3") // HTML Parser
-        // IMPORTANT: Do not bump Jackson above 2.13.1, as newer versions will
-        // break compatibility on older Android devices.
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1") // JSON Parser
     }
 }
